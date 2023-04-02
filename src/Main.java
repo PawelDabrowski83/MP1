1d34634fddba0da8ed1763bc315f61c79a3f3a03
s23551 <s23551@LAB120-20>
2023-04-02 11:08:03 +0200

    Menu continues.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -10,12 +11,14 @@ public class Main {
         String line = "";
         display(Config.MSG_WELCOME);
 
-        Map<Integer, String> menu = constructMenu();
+        Menu menu = new Menu();
 
         try (Scanner scanner = new Scanner(System.in)) {
             display(Config.MSG_CHOOSE);
-            Stream<String> message = menu.values().stream();
-            System.out.println(message);
+            menu.displayMenuOptions();
+            while(!scanner.hasNext()) {
+
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -23,22 +26,9 @@ public class Main {
 
     }
 
-    private static void display(String message) {
+    public static void display(String message) {
         System.out.println(message);
     }
 
-    private static Map<Integer, String> constructMenu() {
-        int menuCounter = 0;
-        Map<Integer, String> menu = new HashMap<>();
-        menu.put(menuCounter, formatMenuItem(Config.OPTION_QUIT, menuCounter++));
-        menu.put(menuCounter, formatMenuItem(Config.OPTION_GENERATE, menuCounter++));
-        menu.put(menuCounter, formatMenuItem(Config.OPTION_CLEAR, menuCounter++));
-        menu.put(menuCounter, formatMenuItem(Config.OPTION_LOAD, menuCounter++));
-        menu.put(menuCounter, formatMenuItem(Config.OPTION_SAVE, menuCounter));
-        return menu;
-    }
 
-    private static String formatMenuItem(String menuItem, int menuNumber) {
-        return String.format(menuItem, menuNumber);
-    }
 }
